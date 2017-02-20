05ea6c8bd1425c0273ad6d919336ee66edaf5bcb
kamontat <kamontat_c@hotmail.com>
2017-02-21 00:29:48 +0700

    add java file to my package

--- src/com/kamontat/Merge.java
+++ src/com/kamontat/Merge.java
@@ -1,4 +1,4 @@
-package com;
+package com.kamontat;
 
 import java.util.*;
 
--- src/com/kamontat/MergeTest.java
+++ src/com/kamontat/MergeTest.java
@@ -1,9 +1,7 @@
-package com;
+package com.kamontat;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import static com.Merge.merge;
 
 /**
  * @author kamontat
@@ -19,8 +17,8 @@ public class MergeTest {
 		int[] a = new int[]{2, 11, 44, 53, 100};
 		int[] b = new int[]{3, 22, 31, 34, 71};
 		
-		Integer[] result = merge(a, b, true);
-		Integer[] resultReverse = merge(a, b, false);
+		Integer[] result = Merge.merge(a, b, true);
+		Integer[] resultReverse = Merge.merge(a, b, false);
 		
 		Assert.assertArrayEquals(expected, result);
 		Assert.assertArrayEquals(expectedReverse, resultReverse);
@@ -31,14 +29,14 @@ public class MergeTest {
 		int[] a = new int[]{3, 11, 34};
 		int[] b = new int[]{2, 22, 31, 44, 53, 71, 100};
 		
-		Integer[] result1 = merge(a, b, true);
-		Integer[] result2 = merge(b, a, true);
+		Integer[] result1 = Merge.merge(a, b, true);
+		Integer[] result2 = Merge.merge(b, a, true);
 		
 		Assert.assertArrayEquals(expected, result1);
 		Assert.assertArrayEquals(expected, result2);
 		
-		Integer[] resultReverse1 = merge(a, b, false);
-		Integer[] resultReverse2 = merge(b, a, false);
+		Integer[] resultReverse1 = Merge.merge(a, b, false);
+		Integer[] resultReverse2 = Merge.merge(b, a, false);
 		
 		Assert.assertArrayEquals(expectedReverse, resultReverse1);
 		Assert.assertArrayEquals(expectedReverse, resultReverse2);
@@ -49,8 +47,8 @@ public class MergeTest {
 		int[] a = new int[]{71, 44, 53, 11, 2};
 		int[] b = new int[]{3, 31, 22, 34, 100};
 		
-		Integer[] result = merge(a, b, true);
-		Integer[] resultReverse = merge(a, b, false);
+		Integer[] result = Merge.merge(a, b, true);
+		Integer[] resultReverse = Merge.merge(a, b, false);
 		
 		Assert.assertArrayEquals(expected, result);
 		Assert.assertArrayEquals(expectedReverse, resultReverse);
@@ -61,14 +59,14 @@ public class MergeTest {
 		int[] a = new int[]{100, 31, 2};
 		int[] b = new int[]{3, 71, 34, 53, 11, 22, 44};
 		
-		Integer[] result1 = merge(a, b, true);
-		Integer[] result2 = merge(b, a, true);
+		Integer[] result1 = Merge.merge(a, b, true);
+		Integer[] result2 = Merge.merge(b, a, true);
 		
 		Assert.assertArrayEquals(expected, result1);
 		Assert.assertArrayEquals(expected, result2);
 		
-		Integer[] resultReverse1 = merge(a, b, false);
-		Integer[] resultReverse2 = merge(b, a, false);
+		Integer[] resultReverse1 = Merge.merge(a, b, false);
+		Integer[] resultReverse2 = Merge.merge(b, a, false);
 		
 		Assert.assertArrayEquals(expectedReverse, resultReverse1);
 		Assert.assertArrayEquals(expectedReverse, resultReverse2);
